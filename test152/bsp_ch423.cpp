13b4267afc91662ede41a3ad673f31d8ac89f37b
AlexGlide <125354026+AlexGlide@users.noreply.github.com>
2023-02-20 15:07:13 +0300

    Update bsp_ch423.cpp

--- test152/bsp_ch423.cpp
+++ test152/bsp_ch423.cpp
@@ -7,43 +7,43 @@ static unsigned char SaveCurrentOCL = 0x00;
 static unsigned char SaveCurrentOCH = 0x00;
 
 void CH423_Init() {
-    bsp_IIC_Init(); //IIC 引脚初始化
-    //置低电平
+    bsp_IIC_Init();                            //Initialize I2C's IO port - As described in "bsp_IIC.h"
+    //Set low
     CH423_WriteByte(CH423_OC_L_CMD);
     CLR_POWER_EN_8_CHAN;
     CLR_POWER_EN_12_CHAN;
 
     CH423_WriteByte(CH423_SYS_CMD | BIT_IO_OE);
-    //置高电平
+    //Set high
     CH423_WriteByte(CH423_SET_IO_CMD | BIT_IO0_DAT | BIT_IO1_DAT | BIT_IO7_DAT);
     CH423_WriteByte(CH423_OC_H_CMD   | BIT_OC8_L_DAT);
 }
 
-void CH423_Write(unsigned short cmd) {    // 写命令
-    IIC_Start();    // 启动总线
+void CH423_Write(unsigned short cmd) {         //Write command
+    IIC_Start();                               //Start the I2C bus (Send the begin signal of I2C - As described in "bsp_IIC.h")
     IIC_Send_Byte(((unsigned char)(cmd>>7) & CH423_I2C_MASK) | CH423_I2C_ADDR1);
     IIC_Wait_Ack();
-    IIC_Send_Byte((unsigned char) cmd);        // 发送数据
+    IIC_Send_Byte((unsigned char) cmd);        //Send data (I2C send a byte)
     IIC_Wait_Ack();
-    IIC_Stop();    // 结束总线
+    IIC_Stop();                                //Stop the I2C bus (Send the stop signal of I2C - As described in "bsp_IIC.h") 
 }
 
-void CH423_WriteByte(unsigned short cmd) {    // 写出数据
-    IIC_Start();    // 启动总线
+void CH423_WriteByte(unsigned short cmd) {     //Write data
+    IIC_Start();                               //Start the I2C bus (Send the begin signal of I2C - As described in "bsp_IIC.h")
     IIC_Send_Byte((unsigned char)(cmd>>8));
     IIC_Wait_Ack();
-    IIC_Send_Byte((unsigned char) cmd);        // 发送数据
+    IIC_Send_Byte((unsigned char) cmd);        //Send data (I2C send a byte) 
     IIC_Wait_Ack();
-    IIC_Stop();    // 结束总线
+    IIC_Stop();                                //Stop the I2C bus (Send the stop signal of I2C - As described in "bsp_IIC.h")
 }
 
-unsigned char CH423_ReadByte() {  // 读取数据
+unsigned char CH423_ReadByte() {               //Read data
     unsigned char din=0;
-    IIC_Start();    // 启动总线
-    IIC_Send_Byte(CH423_RD_IO_CMD);      // 此值为0x4D
+    IIC_Start();    //Start the I2C bus (Send the begin signal of I2C - As described in "bsp_IIC.h")
+    IIC_Send_Byte(CH423_RD_IO_CMD);            //This value is 0x4D
     IIC_Wait_Ack();
-    din=IIC_Read_Byte(0);               // 读取数据
-    IIC_Stop();                         // 结束总线
+    din=IIC_Read_Byte(0);                      //Read data
+    IIC_Stop();                                //Stop the I2C bus (Send the stop signal of I2C - As described in "bsp_IIC.h")
     return (din);
 }
 
