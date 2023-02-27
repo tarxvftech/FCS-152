f235fda852e200a35653b338cbe199593c04498d
AlexGlide <125354026+AlexGlide@users.noreply.github.com>
2023-02-27 12:02:38 +0300

    Update bsp_iic.h

--- test152/bsp_iic.h
+++ test152/bsp_iic.h
@@ -17,15 +17,15 @@
 #define IIC_SDA1            digitalWrite(SDA_PIN, HIGH)
 #define READ_SDA            digitalRead(SDA_PIN)    //Read the input SDA
 
-//IIC All operation functions
+//I2C All operation functions
 void bsp_IIC_Init(void);                    //Initialize I2C's IO port
 void IIC_Start(void);                       //Send the begin signal of I2C
 void IIC_Stop(void);                        //Send the stop  signal of I2C
-void IIC_Send_Byte(uint8_t txd);            //IIC send a byte
-uint8_t IIC_Read_Byte(unsigned char ack);   //IIC read a byte
-uint8_t IIC_Wait_Ack(void);                 //IIC wait for ACK
-void IIC_Ack(void);                         //IIC send an  ACK
-void IIC_NAck(void);                        //IIC send an  NACK
+void IIC_Send_Byte(uint8_t txd);            //I2C send a byte
+uint8_t IIC_Read_Byte(unsigned char ack);   //I2C read a byte
+uint8_t IIC_Wait_Ack(void);                 //I2C wait for ACK
+void IIC_Ack(void);                         //I2C send an  ACK
+void IIC_NAck(void);                        //I2C send an  NACK
 
 
 #endif
