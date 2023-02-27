a4239b3020d0c8c21a00b9f395f0cfacccc62a4a
AlexGlide <125354026+AlexGlide@users.noreply.github.com>
2023-02-27 16:12:08 +0300

    Update bsp_uart.h

--- test152/bsp_uart.h
+++ test152/bsp_uart.h
@@ -6,13 +6,13 @@
 extern volatile unsigned char  rx1_buf[USART1_BUF_SIZE];
 extern volatile unsigned char  rx2_buf[USART2_BUF_SIZE];
 
-//串口1 KDU/固件升级
+//Serial port 1 KDU/firmware upgrade
 void bsp_UART1_Init(int baud);
-void UART1_EnRCV(void);                     //启动串口1接收
-int  UART1_getRcvFlag(void);                //判断接收标志位
-int  UART1_dataPreProcess(void);            //数据预处理
+void UART1_EnRCV(void);                     //Start serial port 1 to receive
+int  UART1_getRcvFlag(void);                //Determine the receiving flag
+int  UART1_dataPreProcess(void);            //Data preprocessing
 
-//串口2 A20模块
+//Serial port 2 A20 module
 void bsp_UART2_Init(int baud);
 
 void UART1_Put_Char(unsigned char ch);
@@ -27,6 +27,6 @@ void UART2_Send_Message(char s[], int size);
 void Set_A20(CHAN_ARV set, uint8_t sq);
 void Set_A20_MIC(uint8_t miclvl, uint8_t scramlvl, uint8_t tot);
 int  Get_A20_RSSI(void);
-void A002_CALLBACK(void);           //A20数据交互,必须处理了才有a20数据返回
+void A002_CALLBACK(void);           //A20 data interaction, must be processed before a20 data is returned
 
 #endif
