8eb072e727fa57256cba89cb2899f693a43ab807
AlexGlide <125354026+AlexGlide@users.noreply.github.com>
2023-02-28 11:26:12 +0300

    Update controller.cpp

--- test152/controller.cpp
+++ test152/controller.cpp
@@ -6,14 +6,14 @@
 
 extern void SHUT(void);
 extern uint8_t SQL;
-void EN_GPIO_Init(void) {       //使能引脚初始化
+void EN_GPIO_Init(void) {       //Enable pin initialization
     switch (Select_Power()) {
     case VOLTAGE_NORMAL:
         D_printf("Normal voltage startup\n");
         return;
 
     case VOLTAGE_ERROR:
-        D_printf("电压错误\n");
+        D_printf("电压错误\n");  //Voltage error
         LCD_Clear(GLOBAL32);
         LCD_ShowString0608(25, 2,  "Wrong Power", 1, 128);
         delay_ms(2000);
@@ -25,7 +25,7 @@ void EN_GPIO_Init(void) {       //使能引脚初始化
     }
     SHUT();
 }
-void A002_Init(void) {          //A20控制引脚初始化
+void A002_Init(void) {          //A20 Control pin initialization
     bsp_A002_Init();
     A002_PD_SET;
     delay_ms(2);
@@ -48,12 +48,13 @@ u8 Select_Power(void) {
     // for(u8 i=0; i<5; i++)
     //     Use_ADC();
     uint32_t adc_val = Use_ADC();
-    Serial.printf("\n当前电压:%d\n", adc_val);
+    Serial.printf("\n当前电压:%d\n", adc_val);  //Current voltage
 
     if (adc_val>VOLTAGE_ON_8_FLOOR && adc_val<VOLTAGE_ON_8_UPPER) {
         POWER_EN_12_SET;
         delay_ms(100);
-        POWER_EN_12_CLR;//硬件错误导致:8V电源电流被12V电源电路瞬间拉低导致电源关
+        POWER_EN_12_CLR;             //Caused by hardware error: the 8V power supply current is instantly pulled down by the 12V power supply circuit, 
+                                     //causing the power to be turned off
         POWER_EN_8_SET;
         POWER_SELECT_FLAG=0;
         return VOLTAGE_NORMAL;
@@ -71,27 +72,27 @@ extern u8 MIC_LEVEL[3], MIC;
 
 void MIC_SWITCH(char mic_temp, char on_off) {
     D_printf("\nMIC:%d, sta:%d\n", mic_temp, on_off);
-    //开运放
-    //设置增益
+    //Open op amp
+    //Set the gain
     if (on_off) {
         switch (mic_temp) {
-        case IN:            //内mic
+        case IN:            //Inside MIC - Radio inside microphone.
             MIC_IN_SET;
             M62364_SetSingleChannel(MIC_IN_CHAN, MIC_LEVEL[1]);
             break;
 
 
-        case TOP:           //顶mic
-        case SIDE:          //侧mic
+        case TOP:           //Top MIC - 6-pin military connector, placed on top of radio. For communication headsets.
+        case SIDE:          //Side MIC
             MIC_OUT_SET;
             M62364_SetSingleChannel(MIC_OUT_CHAN, MIC_LEVEL[MIC]);
             break;
         }
     } else {
-        //关运放
+        //Turn off the OP amp
         MIC_IN_CLR;
         MIC_OUT_CLR;
-        //关矩阵增益
+        //Off matrix gain
         M62364_SetSingleChannel(MIC_IN_CHAN,  0);
         M62364_SetSingleChannel(MIC_OUT_CHAN, 0);
     }
@@ -100,25 +101,25 @@ void MIC_SWITCH(char mic_temp, char on_off) {
 void SPK_SWITCH(char spk_temp, char on_off) {
     D_printf("\nSPK:%d, sta:%d\n", spk_temp, on_off);
     if (on_off) {
-        //开功放
+        //Turn on the power amplifier
         switch (spk_temp) {
-        case IN:            //内spk
+        case IN:            //Inside speaker - Radio inside speaker.
             SPK_IN_SET;
             break;
 
-        case TOP:           //顶spk
-        case SIDE:          //边spk
+        case TOP:           //Top speaker - 6-pin military connector, placed on top of radio. For communication headsets.
+        case SIDE:          //Side speaker
             SPK_OUT_SET;
             break;
         }
     } else {
-        //关功放
+        //Turn off the power amplifier
         SPK_IN_CLR;
         SPK_OUT_CLR;
     }
     delay_ms(1);
 }
-void VDO_SWITCH(unsigned char on_off) { //6针头电源输出
+void VDO_SWITCH(unsigned char on_off) { //6-pin power output - for dynamic (boom) microphone.
     if (on_off) {
         VDO_SET;
         return;
